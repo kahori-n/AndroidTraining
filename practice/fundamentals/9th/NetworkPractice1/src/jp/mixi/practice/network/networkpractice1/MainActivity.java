fbb1806a5821a09e0804f4e38301516227b7e981
kahori.nagasawa <kahori.nagasawa@mixi.co.jp>
2013-05-21 14:43:11 +0900

    実習GETが終わったところ

--- practice/fundamentals/9th/NetworkPractice1/src/jp/mixi/practice/network/networkpractice1/MainActivity.java
+++ practice/fundamentals/9th/NetworkPractice1/src/jp/mixi/practice/network/networkpractice1/MainActivity.java
@@ -1,22 +1,41 @@
 package jp.mixi.practice.network.networkpractice1;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View buttonGet = findViewById(R.id.buttonGet);
+		Log.v(TAG,"onCreate");
+        
         buttonGet.setOnClickListener(new OnClickListener() {
-
-            public void onClick(View v) {
+			public void onClick(View v) {
                 // http getの処理を書く
+				Log.v(TAG,"onClick");
+		        new MyAsyncTask().execute("http://mixi.jp");
             }
         });
         View buttonPost = findViewById(R.id.buttonPost);
@@ -28,11 +47,64 @@ public class MainActivity extends Activity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    /**
+     * 非同期処理を実行するためのネストクラス。
+     * Activity などのライフサイクルに合わせた管理は自分でする必要があるが、
+     * この例では特にしていないので、Activity が GC されると良くないことが起こる。
+     *
+     * ジェネリクスの仕組みを用いて、非同期処理に渡す引数の型、進捗を監視するコールバック用の型、非同期処理の結果を表す型を指定する。
+     *
+     * @author keishin.yokomaku
+     */
+    public class MyAsyncTask extends AsyncTask<String, Void, String> {
+        /**
+         * 非同期処理の本体で、UI スレッドではない別のスレッドで処理する内容。
+         * 引数は非同期処理内容に渡すためのパラメータの配列。
+         */
+        @Override
+        protected String doInBackground(String... params) {
+        	String result = "";
+        	try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = null;
+                try {
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    InputStream is = connection.getInputStream();
 
+                    StringBuilder src = new StringBuilder();
+                    while (true) {
+                        byte[] line = new byte[1024];
+                        int size = is.read(line);
+                        if (size <= 0)
+                            break;
+                        src.append(new String(line, "euc-jp"));
+                    }
+                    
+                    result = src.toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally{
+                    connection.disconnect();
+                }
+            } catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            return result;
+        }
+
+        /**
+         * 非同期処理の実行後に、UI スレッドで実行する処理。
+         * 引数は {@link AsyncTask#execute(Object...)} の返り値。
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+            TextView textView = (TextView) findViewById(R.id.responce);
+            textView.setText(result);
+        }
+
+    }
 }
+
