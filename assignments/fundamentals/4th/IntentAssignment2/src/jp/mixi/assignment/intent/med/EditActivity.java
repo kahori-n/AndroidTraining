055dc070fc0406c4185c9d36c7dda2a81dc8f203
kahori.nagasawa <kahori.nagasawa@mixi.co.jp>
2013-05-21 14:37:30 +0900

    実習まで終わっていたはず

--- assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/EditActivity.java
+++ assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/EditActivity.java
@@ -1,11 +1,16 @@
 package jp.mixi.assignment.intent.med;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditActivity extends Activity {
+    public static final int REQUEST_CODE_HOGE = 1;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +20,13 @@ public class EditActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO ここで、id が MyEdit の EditText からテキストを取得して、結果にセットする
+            	EditText myEdit = (EditText) findViewById(R.id.MyEdit);
+            	String str = myEdit.getText().toString();
+
+            	Intent intent = getIntent();
+            	intent.putExtra("result", str);
+            	setResult(RESULT_OK, intent);
+            	finish();
             }
         });
     }
--- assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/MainActivity.java
+++ assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/MainActivity.java
@@ -2,8 +2,10 @@
 package jp.mixi.assignment.intent.med;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,7 +20,16 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 // TODO EditText と ボタンを 1 つずつ持つ新しい Activity を呼び出し、ボタンを押した時に結果を返すように実装する
                 // TODO 返ってきた結果を Toast で表示するところも実装すること
+            	Intent intent = new Intent(MainActivity.this, EditActivity.class);
+            	startActivityForResult(intent, EditActivity.REQUEST_CODE_HOGE);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // super.onActivityResult(int, int, Intent) の呼び出しは、条件に関係なくすること
+    	super.onActivityResult(requestCode, resultCode, data);
+    	String result = data.getStringExtra("result");
+    	Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
     }
 }
