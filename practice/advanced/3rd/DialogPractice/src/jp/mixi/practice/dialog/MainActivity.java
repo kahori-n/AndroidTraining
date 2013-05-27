30ec61f34112ab789c9c038d623dcd85e67f689b
kahori.nagasawa <kahori.nagasawa@mixi.co.jp>
2013-05-27 12:35:12 +0900

    include practice1, dialog practice1 done

--- practice/advanced/3rd/DialogPractice/src/jp/mixi/practice/dialog/MainActivity.java
+++ practice/advanced/3rd/DialogPractice/src/jp/mixi/practice/dialog/MainActivity.java
@@ -1,12 +1,16 @@
 
 package jp.mixi.practice.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -25,6 +29,8 @@ public class MainActivity extends FragmentActivity {
 
     private void showPracticeDialog() {
         // TODO:ダイアログを表示する処理を実装してください
+    	PracticeDialogFragment myDialogFragment = new PracticeDialogFragment();
+    	myDialogFragment.show(getSupportFragmentManager(), "my_dialog_fragment");
     }
 
     @Override
@@ -36,6 +42,23 @@ public class MainActivity extends FragmentActivity {
 
     // TODO:独自DialogFragmentを実装してください
     public static class PracticeDialogFragment extends DialogFragment {
+    	
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // AlertDialogはBuilderパターンで生成
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+            builder.setMessage(R.string.message)
+            		.setTitle(R.string.practice)
+            		.setIcon(R.drawable.ic_launcher)
+            		// Cancelボタン
+                    .setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(getActivity(), "Negative", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            // Dialogを作成して返却
+            return builder.create();
+        }
     }
 }
--- practice/advanced/3rd/IncludePracticeReport/Report.md
+++ practice/advanced/3rd/IncludePracticeReport/Report.md
@@ -2,3 +2,6 @@ Practice Report for 3.3
 ------
 
 1. (実習) レイアウトを作成しinclude してください。その際に、mergeの使用有無でHierarchy View がどのように表示されるかスクリーンショットをとってレポートしてください。
+
+idをinclude1としてmerge無しのレイアウト,include2としてmerge有りのレイアウトを読み込んだ。
+merge無しの方はid/include1のLinearLayoutが存在するが、merge有りの方はid/include2が無く直接include2内のTextViewが読み込まれている。
